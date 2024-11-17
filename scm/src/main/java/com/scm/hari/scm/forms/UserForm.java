d7e6739af0f3701f6f16ed0057b71da53153f165
Hariom <rajputomi98@gmail.com>
2024-11-17 14:59:32 +0530

    first commit

---
+++ scm/src/main/java/com/scm/hari/scm/forms/UserForm.java
@@ -0,0 +1,20 @@
+package com.scm.hari.scm.forms;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class UserForm {
+    private String name;
+    private String email;
+    private String password;
+    private String about;
+    private String contact;
+}
